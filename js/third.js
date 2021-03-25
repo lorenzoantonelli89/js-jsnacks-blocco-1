749ada9baa836d49dbfeed011040d3e2ec72397b
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-25 22:08:18 +0100

    esercizio finito super bonus

--- js/third.js
+++ js/third.js
@@ -13,36 +13,35 @@ function es1() {
 
     students.push(student);
 
-    tableWrapper.innerHTML = ("<table id='table'><tr><th>Nome</th><th>Cognome</th><th>Età</th><th>REMOVE</th></tr></table> ");
+    tableWrapper.innerHTML = ("<table id='table'><tr><th id='nameorder'>Nome</th><th id='surnameorder'>Cognome</th><th id='ageorder'>Età</th><th>REMOVE</th></tr></table> ");
 
     table = document.getElementById('table');
 
     for(var i = 0; i < students.length; i++){
-
         printTr(students[i]);
-
     }
 }
 
 function printTr(obj) {
     
     var tr = document.createElement('tr');
+    tr.classList.add('remove');
     var tdName = document.createElement('td');
     var tdSurname = document.createElement('td');
     var tdAge = document.createElement('td');
-    var tdCross = document.createElement('td');
+    var tdDelete = document.createElement('td');
     tdName.innerHTML = obj['name'];
     tdSurname.innerHTML = obj['surname'];
     tdAge.innerHTML = obj['age'];
-    tdCross.innerHTML = '<i class="fas fa-times"></i>';
-    tdCross.dataset.index = students.indexOf(obj);
+    tdDelete.innerHTML = '<i class="fas fa-user-slash"></i>';
+    tdDelete.dataset.index = students.indexOf(obj);
     tr.appendChild(tdName);
     tr.appendChild(tdSurname);
     tr.appendChild(tdAge);
-    tr.appendChild(tdCross);
+    tr.appendChild(tdDelete);
     table.appendChild(tr);
 
-    tdCross.addEventListener('click', function () {
+    tdDelete.addEventListener('click', function () {
         this.parentElement.remove();
         students.splice(this.dataset.index, 1);
     });
@@ -62,9 +61,11 @@ function es3() {
     btn.addEventListener('click', function (event) {
 
         var name = document.getElementById('fname').value;
+        name = name.charAt(0).toUpperCase() + name.slice(1);
         var surname = document.getElementById('lname').value;
+        surname = surname.charAt(0).toUpperCase() + surname.slice(1);
         var age = parseInt(document.getElementById('age').value);
-        
+
         event.preventDefault();
 
         student = {
@@ -80,22 +81,65 @@ function es3() {
     });
 }
 
+function printOrder() {
+    var trRem = document.querySelectorAll('#table .remove');
+
+    for (var i = 0; i < students.length; i++) {
+        trRem[i].remove();
+        printTr(students[i]);
+    }
+}
+
 function order() {
-    var btnOrder = document.getElementById('btnorder');
+    
+    var nameOrder = document.getElementById('nameorder');
 
-    btnOrder.addEventListener('click', function () {
+    nameOrder.addEventListener('click', function () {
+        students.sort(function (a, b) {
+            var textA = a.name.toUpperCase();
+            var textB = b.name.toUpperCase();
+            if (textA < textB) {
+                return -1;
+            }
+            if (textA > textB) {
+                return 1;
+            }
+        });
+        printOrder();
+    });
 
-        students.sort(function(a, b){
+    var surnameOrder = document.getElementById('surnameorder');
+
+    surnameOrder.addEventListener('click', function () {
+
+        students.sort(function (a, b) {
             var textA = a.surname.toUpperCase();
             var textB = b.surname.toUpperCase();
-            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-            
+            if (textA < textB) {
+                return -1;
+            }
+            if (textA > textB) {
+                return 1;
+            }
         });
+        printOrder();
+    });
 
-        console.log(students);
+    var ageOrder = document.getElementById('ageorder');
 
-        // printTr(student);
+    ageOrder.addEventListener('click', function () {
         
+        students.sort(function (a, b) {
+            var numA = a.age;
+            var numB = b.age;
+            if (numA < numB){
+                return -1;
+            }else if (numA > numB) {
+                return 1;
+            }
+            return 0;
+        });
+        printOrder();
     });
 }
 
