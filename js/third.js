7417dac94939c079faad56df59e9e80b9ca23548
Lorenzo <lorenzo.antonelli89@gmail.com>
2021-03-26 17:12:27 +0100

    aggiunti commenti

--- js/third.js
+++ js/third.js
@@ -2,15 +2,14 @@ var tableWrapper = document.getElementById('content-table');
 var table;
 var students = [];
 var student;
-
-function es1() {
+// funzione stampa iniziale table e primo studente 
+function printTable() {
 
     var student = {
         'name': 'Lorenzo',
         'surname': 'Antonelli',
         'age': 32
     }
-
     students.push(student);
 
     tableWrapper.innerHTML = ("<table id='table'><tr><th id='nameorder'>Nome</th><th id='surnameorder'>Cognome</th><th id='ageorder'>Età</th><th>REMOVE</th></tr></table> ");
@@ -21,7 +20,7 @@ function es1() {
         printTr(students[i]);
     }
 }
-
+// funzione per stampare ad ogni click del form lo studente in tabella
 function printTr(obj) {
     
     var tr = document.createElement('tr');
@@ -46,41 +45,41 @@ function printTr(obj) {
         students.splice(this.dataset.index, 1);
     });
 }
-
+// funzione per resettare il form dopo submit
 function resetForm() {
     
     document.getElementById('fname').value = '';
     document.getElementById('lname').value = '';
     document.getElementById('age').value = '';
 }
-
-function es3() {
+// funzione click del submit
+function clickSubmit() {
 
     var btn = document.getElementById('submit');
 
     btn.addEventListener('click', function (event) {
 
         var name = document.getElementById('fname').value;
-        name = name.charAt(0).toUpperCase() + name.slice(1);
+        name = name.charAt(0).toUpperCase() + name.slice(1); //mette grande la prima lettera e aggiunge il resto della str
         var surname = document.getElementById('lname').value;
         surname = surname.charAt(0).toUpperCase() + surname.slice(1);
         var age = parseInt(document.getElementById('age').value);
+        var nameReturn = document.getElementById('fname');
 
-        event.preventDefault();
+        event.preventDefault(); // ferma il refresh della pagina dopo invio del form
 
         student = {
             name,
             surname,
             age 
         }
-
         students.push(student);
-
         printTr(student);
         resetForm();
+        nameReturn.focus(); // funzione per far ritornare a inserimento name dopo invio dati
     });
 }
-
+// funzione elimina le vecchie tr e stampa le nuove con nomi ordinati
 function printOrder() {
     var trRem = document.querySelectorAll('#table .remove');
 
@@ -89,7 +88,7 @@ function printOrder() {
         printTr(students[i]);
     }
 }
-
+// funzione per ordinare oggetti dentro array
 function itemsOrder(str) {
     students.sort(function (a, b){
         var A = a[str];
@@ -103,7 +102,7 @@ function itemsOrder(str) {
     });
     printOrder();
 }
-
+// funziona che ordina gli studenti in base al click delle th
 function order() {
     
     var nameOrder = document.getElementById('nameorder');
@@ -127,8 +126,8 @@ function order() {
 
 
 function init() {
-    es1();
-    es3();
+    printTable();
+    clickSubmit();
     order();
 }
 
